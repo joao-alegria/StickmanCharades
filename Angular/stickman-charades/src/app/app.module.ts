aa6a9a4d8fbd5e20133dc895938aa4663c76ca38
Filipe <filipesnetopires@ua.pt>
2020-04-27 10:47:15 +0000

    Basic skeleton of new web ui added.

--- Angular/stickman-charades/src/app/app.module.ts
+++ Angular/stickman-charades/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './elements/footer/footer.component';
+import { PreloaderComponent } from './elements/preloader/preloader.component';
+import { HeaderComponent } from './elements/header/header.component';
+import { BannerComponent } from './elements/banner/banner.component';
+import { TeamComponent } from './elements/team/team.component';
+import { FeaturesComponent } from './elements/features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    PreloaderComponent,
+    HeaderComponent,
+    BannerComponent,
+    TeamComponent,
+    FeaturesComponent,
   ],
   imports: [
     BrowserModule
