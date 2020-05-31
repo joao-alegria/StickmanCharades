c71ca8f38bd85f6fa538fe7f86c40f89f971d593
aspedrosa <aspedrosa@ua.pt>
2020-05-31 17:37:37 +0100

    Add swagger

--- Server/session_commands/src/main/java/pt/ua/deti/es/g54/security/WebSecurityConfig.java
+++ Server/session_commands/src/main/java/pt/ua/deti/es/g54/security/WebSecurityConfig.java
@@ -21,7 +21,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
-            .authorizeRequests().anyRequest().authenticated()
+            .authorizeRequests()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/session/**").authenticated()
             .and().httpBasic().and().authenticationProvider(new CustomAuthenticationProvider());
     }
 
